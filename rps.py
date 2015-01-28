d96db5b685b13732c4a39c163a714a2dceb988d3
Lewis Menelaws <lmenelaws112@gmail.com>
2015-01-28 12:51:23 -0500

    Initial commit for a basic command line of RPS

---
+++ rps.py
@@ -0,0 +1,89 @@
+import time
+import random
+
+hands = ['rock', 'paper', 'scissors']
+
+
+def gamePhase():
+	selection = True
+	while selection == True:
+		userInput = raw_input("Rock, Paper or Scissors? ")
+		if userInput.lower() == "rock" or userInput.lower() == "paper" or userInput .lower() == "scissors":
+			print("good job!")
+			computerChoice = random.choice(hands)
+			print("Results in 3...")
+			time.sleep(1)
+			print("Results in 2...")
+			time.sleep(1)
+			print("Results in 1...")
+			time.sleep(1)
+			if userInput.lower() == computerChoice:
+				print("It's a tie game! Your choice: " + userInput + " The Computers choice: " + computerChoice)
+				time.sleep(3)
+				ask = raw_input("Would you like to play again? y/n")
+				elif ask == 'y':
+					gamePhase()
+				elif ask == 'n':
+					selection = False
+			elif userInput.lower() == 'rock' and computerChoice == 'scissors':
+				print("You win! Your choice: " + userInput + " The Computers choice: " + computerChoice)
+				time.sleep(3)
+				ask = raw_input("Would you like to play again? y/n")
+				elif ask == 'y':
+					gamePhase()
+				elif ask == 'n':
+					selection = False
+			elif userInput.lower() == 'rock' and computerChoice == 'paper':
+				print("You lose! Your choice: " + userInput + " The Computers choice: " + computerChoice)
+				time.sleep(3)
+				ask = raw_input("Would you like to play again? y/n")
+				elif ask == 'y':
+					gamePhase()
+				elif ask == 'n':
+					selection = False
+			elif userInput.lower() == 'paper' and computerChoice == 'rock':
+				print("You win! Your choice: " + userInput + " The Computers choice: " + computerChoice)
+				time.sleep(3)
+				ask = raw_input("Would you like to play again? y/n")
+				elif ask == 'y':
+					gamePhase()
+				elif ask == 'n':
+					selection = False
+			elif userInput.lower() == 'paper' and computerChoice == 'scissors':
+				print("You lose! Your choice: " + userInput + " The Computers choice: " + computerChoice)
+				time.sleep(3)
+				ask = raw_input("Would you like to play again? y/n")
+				elif ask == 'y':
+					gamePhase()
+				elif ask == 'n':
+					selection = False
+			elif userInput.lower() == 'scissors' and computerChoice == 'rock':
+				print("You lose! Your choice: " + userInput + " The Computers choice: " + computerChoice)
+				time.sleep(3)
+				ask = raw_input("Would you like to play again? y/n")
+				elif ask == 'y':
+					gamePhase()
+				elif ask == 'n':
+					selection = False
+			elif userInput.lower() == 'scissors' and computerChoice == 'paper':
+				print("You win! Your choice: " + userInput + " The Computers choice: " + computerChoice)
+				time.sleep(3)
+				ask = raw_input("Would you like to play again? y/n")
+				elif ask == 'y':
+					gamePhase()
+				elif ask == 'n':
+					selection = False
+
+
+
+gamePhase()
+
+
+
+
+
+
+
+
+
+
